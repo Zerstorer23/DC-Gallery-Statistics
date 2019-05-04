784470fcd2de94cfc92ea5a3ba7a266eec278b06
Zerstorer23 <43217966+Zerstorer23@users.noreply.github.com>
2019-05-04 20:08:39 +0900

    Add files via upload

--- Objects/Hash_storage.java
+++ Objects/Hash_storage.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Hash_storage {
-    public static HashMap<String,Integer> UserHash = new HashMap<>();
-    public static ArrayList<User> Userlist = new ArrayList<>();
-    static ArrayList<String> foundUsers = new ArrayList<>();
-    public static Month[][] months = new Month[3][12];
+    public static HashMap<String,Integer> UserHash = new HashMap<>(); // 아래의 유저리스트에서 유저 index를 빨리찾게 돕는 해쉬
+    public static ArrayList<User> Userlist = new ArrayList<>(); //유저 리스트
+    static ArrayList<String> foundUsers = new ArrayList<>(); //뭔지 까먹음
+    public static Month[][] months = new Month[3][12];//3 12값은 설정단계에서 알아서 바뀝니다.
                                       //[Year:(갤러리 설립년도 = 0)] [Month: 1월 = 0]
 
     public static HashMap<String,Integer> vocabHash = new HashMap<>();
